9c93fa4229e7b742a9fad7f0ac7ac23b63843470
Ashish Gour <ashishgour1102@gmail.com>
2023-02-15 18:14:05 +0530

    first commit

---
+++ shareme_backend/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+  name: "save",
+  type: "document",
+  title: "Save",
+  fields: [
+    {
+      name: "savedBy",
+      type: "user",
+      title: "SavedBy",
+    },
+    {
+      name: "save",
+      type: "string",
+      title: "Save",
+    },
+  ],
+};
