0d84f5ac22485e60197a04878a8ee1ce8e836bec
Ashish Gour <ashishgour@Ashishs-Air.Dlink>
2023-04-19 22:58:10 +0530

    Fixed Sanity Not Returning Updated Data Consistently

--- shareme_frontend/src/client.js
+++ shareme_frontend/src/client.js
@@ -1,11 +1,14 @@
-import sanityClient from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url';
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 
+// Since Sanity Query Does Not Return Updated Data Consistently, we are using 'useCdn: false'.
+// More info - https://stackoverflow.com/questions/74214001/sanity-query-does-not-return-updated-data-consistently
+// More info - https://www.sanity.io/docs/api-cdn#da702944cd86
 export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  dataset: 'production',
-  apiVersion: '2021-11-16',
-  useCdn: true,
+  dataset: "production",
+  apiVersion: "2021-11-16",
+  useCdn: false,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
